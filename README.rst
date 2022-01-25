d0191d5069cd8ae7aa040c8d3050956a441a2423
Foad Lind <foad.lind@citynetwork.eu>
2022-01-25 09:38:26 +0100

    Create tutor-contrib-webhook-receiver plugin
    Created from Tutor plugin cookiecutter:
    https://github.com/overhangio/cookiecutter-tutor-plugin

---
+++ README.rst
@@ -0,0 +1,22 @@
+webhook-receiver plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/hastexo/tutor-contrib-webhook-receiver
+
+Usage
+-----
+
+::
+
+    tutor plugins enable webhook-receiver
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ tutor_webhook_receiver/plugin.py
@@ -0,0 +1,26 @@
+from glob import glob
+import os
+import pkg_resources
+
+from .__about__ import __version__
+
+templates = pkg_resources.resource_filename(
+    "tutor_webhook_receiver", "templates"
+)
+
+config = {}
+
+hooks = {}
+
+
+def patches():
+    all_patches = {}
+    patches_dir = pkg_resources.resource_filename(
+        "tutor_webhook_receiver", "patches"
+    )
+    for path in glob(os.path.join(patches_dir, "*")):
+        with open(path) as patch_file:
+            name = os.path.basename(path)
+            content = patch_file.read()
+            all_patches[name] = content
+    return all_patches
